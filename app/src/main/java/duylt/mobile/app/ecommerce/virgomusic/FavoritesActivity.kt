3ef39a6639fa900cb887cdf7468bd6de11e62cdb
admin <lothanhduy2003@gmail.com>
2024-02-22 23:56:18 +0700

    create ui play favorite screen

---
+++ app/src/main/java/duylt/mobile/app/ecommerce/virgomusic/FavoritesActivity.kt
@@ -0,0 +1,11 @@
+package duylt.mobile.app.ecommerce.virgomusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+    }
+}
