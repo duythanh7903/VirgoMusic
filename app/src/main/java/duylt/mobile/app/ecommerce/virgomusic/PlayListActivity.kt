f161be0872441e38ba7866bef284aba45628ab15
admin <lothanhduy2003@gmail.com>
2024-02-22 23:51:41 +0700

    create ui play list screen

---
+++ app/src/main/java/duylt/mobile/app/ecommerce/virgomusic/PlayListActivity.kt
@@ -0,0 +1,11 @@
+package duylt.mobile.app.ecommerce.virgomusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_list)
+    }
+}
